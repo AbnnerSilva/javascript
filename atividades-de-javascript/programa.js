e30b6cf9a5acae85af2ec33cb8dc9fd0f88ad542
Abnner Silva <98194806+AbnnerSilva@users.noreply.github.com>
2026-07-21 10:52:54 -0300

    aprendendo java script
    estou estudando com obsidian e videos do youtube

---
+++ atividades-de-javascript/programa.js
@@ -0,0 +1,7 @@
+const x = 10;
+const y = 2.5;
+const nome = "jesus eu te amo, obrigado por cuidar de mim <3"
+
+const sobrenome = 'abnner quer deixar jesus contente com o comportamento dele';
+
+console.log(x);
