11fb8a381afa0fe39c162520d268eef7dfccb252
AbnnerSilva <abnnersilva9@gmail.com>
2026-08-04 15:36:06 -0300

    feat revisão e novo assunto

--- atividades-de-javascript/programa.js
+++ atividades-de-javascript/programa.js
@@ -10,3 +10,26 @@ console.log(frutas[0]);
 console.log(frutas[1]);
 
 console.log()
+
+
+/* aprendendo if e else */
+
+let nota = 7 ;
+let notaminipassar = 6;
+
+if( nota > notaminipassar) {
+    console.log("aluno passou")
+} else {
+    console.log(" o aluno não passou")
+}
+
+
+let frutas = ["banana", "kiwi", "ameixa", "melancia"]
+
+if (frutas.includes("abacate") == true){
+    console.log("a lista está completa")
+
+} else {
+    console.log("está faletando abacate")
+}
+
